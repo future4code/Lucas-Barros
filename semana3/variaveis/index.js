8b286d1f6cd0b828925b435b9c5b8adc3eb28c79
lucasbarroscosta <lucasbarroscosta@gmail.com>
2021-02-22 15:32:51 -0300

    concluindo exercicios

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,70 @@
+// **Exercícios de interpretação de código**
+
+// 1. console mostrará 10 e com valor 5 aparecera 5 (acredito que como na aula a professora disse se não usar o "let" dará conflito)
+// 2. a=10, b=20 , c=10
+
+
+
+
+/*let nome = "" ;
+console.log(typeof nome)
+
+let idade = "" ;
+console.log(typeof idade)*/
+
+/*let nome = prompt("qual o seu nome")
+console.log(nome)
+let idade = prompt("qual é sua idade")
+console.log(idade)*/
+
+/*let nome = prompt("qual o seu nome")
+let idade = prompt ("qual sua idade")
+console.log('Ola',nome,'você tem', idade,'anos',)*/
+
+
+// d) Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.
+// apareceu no console string, acredito que isso tenha acontecido por eu ter usado "",
+
+
+//2. Faça um programa que faça 5 perguntas para o usuário (pode ser criativo aqui). Imprima-as com as respostas no console da seguinte forma:
+
+
+/**let endereco = prompt("qual o seu Endereço")
+console.log('resposta:',endereco)
+
+let escola = prompt("qual a melhor escola de programação")
+console.log('resposta:',escola)
+
+let cor = prompt("qual sua cor favorita")
+console.log('resposta:',cor)
+
+let carro = prompt("qual o seu carro")
+console.log('resposta:',carro)
+
+let ide = prompt("qual o seu IDE favorito")
+console.log('resposta:',ide)*/
+
+//3. Faça um programa que contenha um array com 5 das suas comidas preferidas, armazenado em uma variável. Em seguida, siga os passos:
+
+/*let comidas = ['pizza','cachorro-quente','lasanha','esfiha','churrasco']
+console.log(comidas)
+
+console.log('Essas são as minhas comidas preferidas:',comidas)
+
+let preferida = prompt ("qual sua comida preferida?")
+
+
+comidas[2] = preferida
+
+console.log(comidas)*/
+
+// 4. Faça um programa que contenha um array com 3 perguntas de Sim ou Não, armazenado em uma variável. Por exemplo: "Você está usando uma roupa azul hoje?". Depois, siga os passos:
+
+let perguntas = ['vai chover hoje?', 'você tem celular?', 'você gosta do Brasil?']
+
+let respostas = [true, false, true]
+
+console.log(perguntas[0], respostas[0])
+console.log(perguntas[1], respostas[1])
+console.log(perguntas[2], respostas[2])
+
