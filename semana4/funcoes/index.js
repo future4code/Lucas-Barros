8b151123590bfad117638c798ca1cf0d1daaa26d
lucasbarroscosta <lucasbarroscosta@gmail.com>
2021-03-01 16:23:26 -0300

    Treinando funcoes

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,57 @@
+// exercicio 1
+//a. O que vai ser impresso no console? Resposta 10,50
+//b. b. O que aconteceria se retirasse os dois `console.log` 
+//e simplesmente invocasse a função `minhaFuncao(2)` e `minhaFuncao(10)`? 
+//O que apareceria no console?
+// resposta: Nada
+
+
+//Exercicio 2
+// a. Explicite quais são as saídas impressas no console
+//Resposta: Darvas e Caio, foram essas a saida porque no codigo do loop pede para começar no 0 e o i < 2, no caso
+//[2]João não vai aparecer
+//b. Se `arrayDeNomes` mudasse de valor para `["Amanda", "Caio"]`, o que vai ser impresso no console?
+//Resposta: amanda e Caio, pois o loop sempre só pede dois
+
+
+//Exercicio 3
+//
+
+
+//Exercicio 4
+
+/*function sobreMim() {
+    console.log("Eu sou Lucas, tenho 30 anos, moro em São Paulo e sou estudante")
+}
+
+sobreMim()*/
+
+
+//b.
+
+/*function sobreMim1(nome, idade, endereco, estudante) { 
+    
+    console.log("eu sou", nome, "tenho", idade, "anos", "moro na rua", endereco, "e", estudante,"estudante")
+    idade = Number
+
+    estudante = true
+
+    if (estudante) {
+      
+        console.log("sou")
+    } else {
+       console.log("nao sou")
+    }
+}
+
+
+sobreMim1("lucas", 30, "aracaju", "false" );*/
+
+// Exercicio 5 
+//a. Escreva uma função que receba um array de números e devolva a quantidade de elementos nele
+
+const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+function elemento(array) {
+    
+}
