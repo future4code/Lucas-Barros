3c92954721f3af4e4e1b1b7d034bc30633e7f791
lucasbarroscosta <lucasbarroscosta@gmail.com>
2021-03-23 16:48:51 -0300

    coloquei api, inputs e botoes

---
+++ semana7/cadastro-usuario/cad-usuario/src/components/ListaUsuario.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+
+export default class ListaUsuario extends React.Component {
+
+    render(){
+
+        return(
+            <div></div>
+            )
+    }
+       
+}
